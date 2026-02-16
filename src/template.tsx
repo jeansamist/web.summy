67780b86c03032c0cbb1213f9b0aa3b894a8b420
BAHA Ephraim <jeansamist@gmail.com>
2026-02-16 17:03:04 +0100

    Ad logo

--- src/template.tsx
+++ src/template.tsx
@@ -1,11 +1,11 @@
-import { type TemplateProps } from 'rasengan';
+import { type TemplateProps } from "rasengan";
 
 export default function Template({ Head, Body, Script }: TemplateProps) {
   return (
     <html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/rasengan.svg" />
+        <link rel="icon" type="image/svg+xml" href="/summy-favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
