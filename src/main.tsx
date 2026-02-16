08a85123322ffb33dc028fde570503c761135086
BAHA Ephraim <jeansamist@gmail.com>
2026-02-16 16:20:54 +0100

    add the theme switcher

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import '@rasenganjs/image/css';
-import '@/styles/index.css';
-import { type AppProps } from 'rasengan';
-
+import { ThemeProvider } from "@/components/theme-provider";
+import "@/styles/index.css";
+import { type AppProps } from "rasengan";
 export default function App({ Component, children }: AppProps) {
-  return <Component>{children}</Component>;
+  return (
+    <Component>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        {children}
+      </ThemeProvider>
+    </Component>
+  );
 }
