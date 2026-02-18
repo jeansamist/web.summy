8b0708bbff7eb1a6f42da42ed5c369074db80c6e
BAHA Ephraim <jeansamist@gmail.com>
2026-02-18 17:43:15 +0100

    Add a sign up page

--- src/app/auth/auth.router.ts
+++ src/app/auth/auth.router.ts
@@ -1,11 +1,12 @@
 import { RouterComponent, defineRouter } from "rasengan";
 import AuthLayout from "./auth.layout";
 import SignInPage from "./sign-in.page";
+import SignUpPage from "./sign-up.page";
 
 class AuthRouter extends RouterComponent {}
 
 export default defineRouter({
   imports: [],
   layout: AuthLayout,
-  pages: [SignInPage],
+  pages: [SignInPage, SignUpPage],
 })(AuthRouter);
---
+++ src/app/auth/sign-up.page.tsx
@@ -0,0 +1,12 @@
+import { PageComponent } from "rasengan";
+const SignUpPage: PageComponent = () => {
+  return <main></main>;
+};
+
+SignUpPage.path = "/sign-up";
+SignUpPage.metadata = {
+  title: "Summy - Sign Up - Create a Summy account",
+  description: "Join Summy and start summarizing your life with AI.",
+};
+
+export default SignUpPage;
